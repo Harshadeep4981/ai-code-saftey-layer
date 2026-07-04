6c07cd9a3355ec277b6ef804b9b87721fba8a1d9
harsha <harshadeepm63@gmail.com>
2026-07-04 23:07:45 +0530

    vercel link successfully added

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ai-code-saftey-layer.vercel.app"], 
+    allow_origins="https://ai-code-saftey-layer(-.*)?\.vercel\.app", 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
